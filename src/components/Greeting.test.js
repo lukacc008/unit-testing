f25dcb96ce88216aeeec4d241b1430c2a3eda3d4
unknown <lukacc008@gmail.com>
2024-06-13 16:59:24 +0200

    Making component a little more complex in order to learn different types of testing

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("Renders Hello World as a text", () => {
+describe("Greeting component", () => {
+  test("Renders Hello World as a text", () => {
+    render(<Greeting />);
+    const helloWorldElement = screen.getByText("Hello World", { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+});
+
+
+test("Checks for text in paragraph", () => {
   render(<Greeting />);
-  const helloWorldElement = screen.getByText("Hello World", { exact: false });
-  expect(helloWorldElement).toBeInTheDocument();
+  const paragraphElement = screen.getByText("It's good to see you", { exact: false });
+  expect(paragraphElement).toBeInTheDocument();
 });
