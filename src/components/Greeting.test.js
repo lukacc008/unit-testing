04c144dc69d509a9da4b579243b17b2f3800188e
unknown <lukacc008@gmail.com>
2024-06-14 16:34:29 +0200

    Wrote few more tests for practice purpose

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -9,9 +10,32 @@ describe("Greeting component", () => {
   });
 });
 
-
 test("Checks for text in paragraph", () => {
   render(<Greeting />);
-  const paragraphElement = screen.getByText("It's good to see you", { exact: false });
+  const paragraphElement = screen.getByText("good to see you", {
+    exact: false,
+  });
   expect(paragraphElement).toBeInTheDocument();
+});
+
+test("Renders Changed! if the button was clicked", () => {
+  // 1st step Arrange
+  render(<Greeting />);
+  // 2nd step Act
+  const buttonElement = screen.getByRole("button");
+  userEvent.click(buttonElement);
+  // Assert
+  const outputElement = screen.getByText("Changed!");
+  expect(outputElement).toBeInTheDocument();
+});
+
+test("does not render 'good to see you' if the button was clicked", () => {
+  // 1st step Arrange
+  render(<Greeting />);
+  // 2nd step Act
+  const buttonElement = screen.getByRole("button");
+  userEvent.click(buttonElement);
+  // Assert
+  const outputElement = screen.queryByText("good to see you", { exact: false });
+  expect(outputElement).toBeNull();
 });
